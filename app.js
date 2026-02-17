02e20e45b360cb5fc970733a241408e7270f3a0d
Neeraj Alavala <neeraj@teamtitan.ai>
2026-02-17 01:50:44 -0500

    handle menu logic

--- app.js
+++ app.js
@@ -143,7 +143,60 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
       } catch (err) {
         console.error('Error sending message:', err);
       }
+    } else if (componentId.startsWith('select_choice_')) {
+      // get the associated game ID
+      const gameId = componentId.replace('select_choice_', '');
+  
+      if (activeGames[gameId]) {
+        // Interaction context
+        const context = req.body.context;
+        // Get user ID and object choice for responding user
+        // User ID is in user field for (G)DMs, and member for servers
+        const userId = context === 0 ? req.body.member.user.id : req.body.user.id;
+        const objectName = data.values[0];
+        // Calculate result from helper function
+        const resultStr = getResult(activeGames[gameId], {
+          id: userId,
+          objectName,
+        });
+  
+        // Remove game from storage
+        delete activeGames[gameId];
+        // Update message with token in request body
+        const endpoint = `webhooks/${process.env.APP_ID}/${req.body.token}/messages/${req.body.message.id}`;
+  
+        try {
+          // Send results
+          await res.send({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: {
+              flags: InteractionResponseFlags.IS_COMPONENTS_V2,
+              components: [
+                {
+                  type: MessageComponentTypes.TEXT_DISPLAY,
+                  content: resultStr
+                }
+              ]
+              },
+          });
+          // Update ephemeral message
+          await DiscordRequest(endpoint, {
+            method: 'PATCH',
+            body: {
+              components: [
+                {
+                  type: MessageComponentTypes.TEXT_DISPLAY,
+                  content: 'Nice choice ' + getRandomEmoji()
+                }
+              ],
+            },
+          });
+        } catch (err) {
+          console.error('Error sending message:', err);
+        }
+      }
     }
+    
     return;
   }
 
